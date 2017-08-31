d5bb0ca9c951721f2b898a66d938de44f545e7f8
yatakan <takaya423@outlook.jp>
2017-08-31 14:58:55 +0900

    Facebookログイン機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
   validates :name, :material, presence: true
 
+  ### アソシエーション ###
   has_many :comments, dependent: :destroy
   has_many :article_comments, dependent: :destroy
-
   has_many :records, dependent: :destroy
   has_many :articles, dependent: :destroy
   has_many :articles_likes, dependent: :destroy
@@ -15,6 +15,26 @@ class User < ApplicationRecord
   has_many :like_articles, through: :articles_likes, source: :articles
   has_many :like_records, through: :records_likes, source: :records
 
+
+  ### facebookログイン機能 ###
+  validates :password, presence: false, on: :facebook_login
+
+  def self.from_omniauth(auth)
+      # emailの提供は必須とする
+      user = User.where('email = ?', auth.info.email).first
+    if user.blank?
+      user = User.new
+    end
+  user.uid   = auth.uid
+  user.name  = auth.info.name
+  user.email = auth.info.email
+  user.icon  = auth.info.image
+  user.oauth_token      = auth.credentials.token
+  user.oauth_expires_at = Time.at(auth.credentials.expires_at)
+  user
+  end
+
+  ### フォロー機能 ###
   has_many :active_relationships,  class_name:  "Relationship",
                                    foreign_key: "follower_id",
                                    dependent:   :destroy
