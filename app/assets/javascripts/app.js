795064657bfffc56024e48b796b4efca6f5cea71
yatakan <takaya423@outlook.jp>
2017-09-01 16:38:35 +0900

    GEM font-awesome-rails

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,9 +1,7 @@
 $(document).on('turbolinks:load', function() {
   var href = window.location.href.match(/articles/)
   if(href){
-    reg = "</p>"
-    var article_text = document.getElementById('article-text').innerText.replace(/<p>/,"").replace(reg,"")
-    article_text = article_text.replace(/(#)(.+)/g,"<h2>$2</h2>")
+    var article_text = $('#article-text').html().replace(/(#)(.+)/g,"<h2>$2</h2>")
     $("#article-text").html(article_text)
   }
 });
